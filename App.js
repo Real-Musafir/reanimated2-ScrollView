54a731eee01f81669b089a16507415a1a47c2696
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-26 19:49:12 +0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withRepeat,
+} from "react-native-reanimated";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>asdf</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
