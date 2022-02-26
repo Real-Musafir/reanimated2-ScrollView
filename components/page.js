b7586f2819ed53332e910c66f6df3f25105c3803
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-26 23:37:55 +0600

    add borderRadius in interpulation

--- components/page.js
+++ components/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, Dimensions, StyleSheet } from "react-native";
 import Animated, {
+  Extrapolate,
   interpolate,
   useAnimatedScrollHandler,
   useAnimatedStyle,
@@ -10,11 +11,20 @@ const { height, width } = Dimensions.get("window");
 const SIZE = width * 0.7;
 
 const Page = ({ index, title, translateX }) => {
+  const inputRaneg = [(index - 1) * width, index * width, (index + 1) * width];
   const rStyle = useAnimatedStyle(() => {
     const scale = interpolate(
       translateX.value,
-      [(index - 1) * width, index * width, (index + 1) * width],
-      [0, 1, 0]
+      inputRaneg,
+      [0, 1, 0],
+      Extrapolate.CLAMP
+    );
+
+    const borderRadius = interpolate(
+      translateX.value,
+      inputRaneg,
+      [0, SIZE / 2, 0],
+      Extrapolate.CLAMP
     );
 
     //1st array in input range, 2nd value in output range,
@@ -22,6 +32,7 @@ const Page = ({ index, title, translateX }) => {
     /// when translateX.value=index * width then scale = 1
     /// when translateX.value=(index + 1) * width then scale = 0
     return {
+      borderRadius,
       transform: [{ scale }],
     };
   });
